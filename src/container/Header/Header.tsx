04c91685d74f3c324e63601bb98672befcfacd51
Yaroslav <kznkv.test@gmail.com>
2022-10-14 20:51:49 +0300

    add Menu

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import Container from '@mui/material/Container'
+import Menu from 'components/Menu/Menu'
 
 type Props = {}
 
 const Header = (props: Props) => {
-    return <div>Header</div>
+    return (
+        <>
+            <AppBar position="static">
+                <Container maxWidth="lg">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            variant="h6"
+                            component="div"
+                            sx={{ flexGrow: 1 }}
+                        >
+                            Fake shop
+                        </Typography>
+                        <Menu />
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </>
+    )
 }
 
 export default Header
