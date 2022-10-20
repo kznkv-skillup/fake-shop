4db84b691884b7815dd4856bed424122a5ddad35
Yaroslav <kznkv.test@gmail.com>
2022-10-20 19:57:25 +0300

    add productsArray

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -1,6 +1,15 @@
-import { Grid, Typography } from '@mui/material'
-import React from 'react'
-import ProductListItem from './ProductListItem'
+import { Grid, Typography } from "@mui/material"
+import React from "react"
+import ProductListItem from "./ProductListItem"
+import productsArray from "./productsArray"
+
+type ProductProps = {
+    name: string
+    description: string
+    type: string
+    capacity: number
+    price: number
+}
 
 type Props = {}
 
@@ -11,8 +20,8 @@ const ProductsList = (props: Props) => {
                 align="center"
                 variant="h4"
                 sx={{
-                    margin: '25px 0',
-                    textTransform: 'uppercase',
+                    margin: "25px 0",
+                    textTransform: "uppercase",
                 }}
             >
                 Products List
@@ -24,33 +33,25 @@ const ProductsList = (props: Props) => {
                 alignItems="center"
                 spacing={4}
             >
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem
-                        name="iPhone X"
-                        description="This is iPhone X"
-                        type="phone"
-                        capacity={64}
-                        price={500}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem
-                        name="iPhone XS"
-                        description="This is iPhone XS"
-                        type="phone"
-                        capacity={128}
-                        price={1000}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem
-                        name="iPhone 12 Pro"
-                        description="This is iPhone 12 Pro"
-                        type="phone"
-                        capacity={512}
-                        price={800}
-                    />
-                </Grid>
+                {productsArray.map(
+                    ({
+                        name,
+                        description,
+                        type,
+                        capacity,
+                        price,
+                    }: ProductProps) => (
+                        <Grid item xs={12} sm={6} md={4}>
+                            <ProductListItem
+                                name={name}
+                                description={description}
+                                type={type}
+                                capacity={capacity}
+                                price={price}
+                            />
+                        </Grid>
+                    )
+                )}
             </Grid>
         </>
     )
---
+++ src/components/Products/productsArray.ts
@@ -0,0 +1,55 @@
+type Product = {
+    name: string
+    description: string
+    type: string
+    capacity: number
+    price: number
+}
+
+const productsArray: Product[] = [
+    {
+        name:"iPhone X",
+        description:"This is iPhone X",
+        type:"phone",
+        capacity:64,
+        price:500,
+    },
+    {
+        name:"iPhone 13 Pro",
+        description:"This is iPhone 13Pro",
+        type:"phone",
+        capacity:128,
+        price:1000,
+    },
+    {
+        name:"iPhone XS",
+        description:"This is iPhone XS",
+        type:"phone",
+        capacity:512,
+        price:800,
+    },
+    {
+        name:"iPhone 14 Pro Max",
+        description:"This is iPhone 14 Pro Max",
+        type:"phone",
+        capacity:256,
+        price:200,
+    },
+    {
+        name:"iPhone 8 plus",
+        description:"This is iPhone 8 plus",
+        type:"phone",
+        capacity:64,
+        price:500,
+    },
+    {
+        name:"iPhone 12",
+        description:"This is iPhone 12",
+        type:"phone",
+        capacity:128,
+        price:100,
+    }
+]
+
+
+export default productsArray
