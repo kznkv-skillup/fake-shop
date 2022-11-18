84a6e766643b1b5f611ecbad59c78bebc161a1c3
Yaroslav <kznkv.test@gmail.com>
2022-11-18 17:27:44 +0200

    add CheckoutPage

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,6 +1,7 @@
 import { Container } from "@mui/material"
 import AboutPage from "pages/About/AboutPage"
 import CartPage from "pages/Cart/CartPage"
+import CheckoutPage from "pages/Checkout/CheckoutPage"
 import Home from "pages/Home/Home"
 import PaymentPage from "pages/Payment/PaymentPage"
 import ShippingPage from "pages/Shipping/ShippingPage"
@@ -16,6 +17,7 @@ const Main = () => {
                     <Route path="about" element={<AboutPage />} />
                     <Route path="shipping" element={<ShippingPage />} />
                     <Route path="payment" element={<PaymentPage />} />
+                    <Route path="checkout" element={<CheckoutPage />} />
                 </Routes>
             </Container>
         </main>
